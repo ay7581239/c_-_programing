5615cf8e97cb581e6ce697e7756ec17de3b0f7e5
Abhishek Kumar Yadav <121045931+ay7581239@users.noreply.github.com>
2023-01-26 19:06:43 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+ int n,i,j,k;
+    cin>>n;
+    i=1;
+    while(i<=n)
+    {
+        j=1;
+        while(j<=n-i)
+        {
+            cout<<" ";
+            j++;
+        }
+        j=1;k=i-1;int l=i;
+        while(j<=2*i-1)
+        {
+            if(l>=j)
+            {
+                k++;
+                cout<<k;
+            }
+            else{
+                k--;
+                cout<<k;
+                 }
+        j++;
+        }
+        i++;
+        cout<<endl;
+    }
+    /*
+     1
+   232
+  34543
+ 4567654
+567898765
+*/
+}     
