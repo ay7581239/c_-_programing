baddc08431cf6c45fb54465f93ae94fec963194d
Abhishek Kumar Yadav <121045931+ay7581239@users.noreply.github.com>
2023-02-24 13:30:57 +0530

    Add files via upload

---
+++ sum_or_product.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+    int n,c,pro=1,sum=0;
+    cin>>n>>c;
+    if (c==1)
+    {
+        for (int i = 1; i<=n; i++)
+        {
+           sum=sum+i;
+        }
+        cout<<sum<<endl;
+    }
+    else if (c==2)
+    {
+       for (int i = 1; i<=n; i++)
+        {
+           pro=pro*i;
+        }
+        cout<<pro<<endl; 
+    }
+    else
+    cout<<"-1"<<endl;
+    
+    
+	
+}
