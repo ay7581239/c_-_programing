ee2503b6c2320dd161e7249ed6a0abe5464454c1
Abhishek Kumar Yadav <121045931+ay7581239@users.noreply.github.com>
2023-03-03 16:02:17 +0530

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   long int n,i,c,k;
+    cin>>n;
+    for ( i = 1; i < n; i++)
+    {
+       int c=cin.get();
+    }
+     i=1;
+     while(i<n)
+     {
+        cout<<c;
+     }
+}
